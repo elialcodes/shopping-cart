69cc8bd5c87a34473e372474dded86fb6b12e801
Noelia <abascalcabreran@gmail.com>
2024-07-27 13:10:16 +0200

    stylig detail product

--- src/components/DetailProduct.tsx
+++ src/components/DetailProduct.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import '../styles/DetailProduct.css';
 
 interface DetailProductProps {
   products: AllProducts;
@@ -10,18 +11,18 @@ function DetailProduct({ products }: DetailProductProps): JSX.Element {
   //convertir "id" a número sólo si está definido
   const productId = id ? parseInt(id) : null;
 
-  //verificar si productId es válido antes de buscar el producto
+  //buscar el producto si productId es válido
   const productData = productId
     ? products.find(product => product.id === productId)
     : null;
 
-  //renderizar un mensaje o un componente diferente si "productData" es null
+  //renderizar un mensaje si "productData" es null
   if (!productData) {
     return <div>Producto no encontrado</div>;
   }
 
   return (
-    <>
+    <main>
       <div className="product">
         <img src={productData.thumbnail} alt={productData.title} />
         <div>
@@ -31,7 +32,7 @@ function DetailProduct({ products }: DetailProductProps): JSX.Element {
         <div>{productData.description}</div>
         <div>Rating: {productData.rating}</div>
       </div>
-    </>
+    </main>
   );
 }
 
